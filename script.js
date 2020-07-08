cec465d05d73da52f945233cde0778ffb546d505
Hikikamaru <gayzzz228@gmail.com>
2020-07-08 12:34:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+" use strict ";
+let gg = 2;
+gg=100;
+console.log(gg);
+//alert('Hello');
+
+
+//const result = confirm ("Ponchyk shytnik ?");
+
+
+//const answer = [];
+//answer[0] = prompt("Скільки вам років?",''); 
+//answer[1] = prompt("Як вас звати?",'');
+///answer[2] = prompt("Напишіть свою фамілію",'');
+//answer[3] = prompt("В якому в класі?",'');
+//console.log(answer);
+const user = "кристувач";
+const qusion = confirm (`Привіт,${user},ти вперше в насна сайті ? `);
+console.log(qusion);
